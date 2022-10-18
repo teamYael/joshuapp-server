c2b87e1c172289c3b0b33b7aea63aa917aa5bb5c
Josu Etxeberria <josu.echeverria@ikasle.aeg.eus>
2022-10-18 10:34:34 +0200

    [ADD] enviroment variables

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`Hello ${process.env.ROL_JOSHUA}`)
 })
 
 app.listen(PORT, () => {
