6db50b519a301538f25a4b052f16e569deb7f2f8
OIER ANDUEZA ARTOLA <oier.andueza@ikasle.aeg.eus>
2022-10-21 09:47:19 +0200

    [UPDATE] verifyToken route

--- src/routes/workoutRoutes.js
+++ src/routes/workoutRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const {verifyToken} = require('../middleware/verifyToken');
 
 const userController = require('../controllers/workoutController');
 
@@ -7,7 +8,7 @@ router.get("/", userController.getAllUsers);
 
 router.get("/:userId", userController.getOneUser);
 
-router.post("/token", middleware, userController.createNewUser);
+router.post("/token", verifyToken, userController.createNewUser);
 
 router.patch("/:userId", userController.updateOneUser);
 
