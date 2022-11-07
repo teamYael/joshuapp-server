be00e82ba48c05c0dffe4ff001f916bd6c9e32ab
UnaiAlfaroAEG <unai.alfaro@ikasle.aeg.eus>
2022-11-07 10:49:55 +0100

    [NEW] Nuevo Patch acolitstate

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -16,6 +16,8 @@ router.post("/token", /*verifyToken,*/ verifyEmail, userController.loginUser);
 
 router.patch("/users/:userId", userController.updateOneUser);
 
+router.patch("/acolitstate/:userEmail", userController.updateUserActive);
+
 router.delete("/users/:userId", userController.deleteOneUser);
 
 module.exports = router;
