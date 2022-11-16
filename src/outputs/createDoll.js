fcd5a910cef7dfba294a403ee189c376ccdf6a35
Yael Martinez <yael.martinez@ikasle.aeg.eus>
2022-11-16 13:10:23 +0100

    Style changes

--- src/outputs/createDoll.js
+++ src/outputs/createDoll.js
@@ -1,20 +1,22 @@
- const arrayPieces = [{
-    name:'leg',
-    photo:"require('../../../assets/leg.jpg')"
-},
-{
-    name:'arm',
-    photo:"require('../../../assets/arm.jpg')"
-},
-{
-    name:'head',
-    photo:"require('../../../assets/head.jpg')"
-},
-{
-    name:'body',
-    photo:"require('../../../assets/body.jpg')"
-},
+ const arrayPieces = [
+    {
+        name:'leg',
+        photo:"require('../../../assets/leg.jpg')"
+    },
+    {
+        name:'arm',
+        photo:"require('../../../assets/arm.jpg')"
+    },
+    {
+        name:'head',
+        photo:"require('../../../assets/head.jpg')"
+    },
+    {
+        name:'body',
+        photo:"require('../../../assets/body.jpg')"
+    },
 ];
+
 module.exports = {
     arrayPieces
 }
