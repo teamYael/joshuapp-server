8395b6c04b5ade7fcb1fe9bf91cce670af83a436
Josu Etxeberria <josu.echeverria@ikasle.aeg.eus>
2022-11-30 10:26:42 +0100

    [FIX] error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const mongodbRoute = `mongodb+srv://:${process.env.PASSWORD}${process.env.DBUSERNAME}@e4p1.zrnbvtk.mongodb.net/JoshuApp`;
 //Josu `mongodb+srv://${process.env.DBUSERNAME}:${process.env.PASSWORD}@e4p1.gye2wcf.mongodb.net/JoshuApp'
@@ -24,7 +24,7 @@ const io = socketIO(server, {
 });
 exports.socketIO = io;
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
@@ -32,13 +32,12 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 })
+app.use("/api", userRouter);
+app.use("/api", dollRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to Absentia Staging Socket Service from CI')
 })
-
-app.use("/api", userRouter);
-app.use("/api", dollRouter);
 
 async function start() {
   try {
