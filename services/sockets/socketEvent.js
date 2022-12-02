cede2083e2a11ce4d480f189e4ea9a94d9c67ae3
Josu Etxeberria <josu.echeverria@ikasle.aeg.eus>
2022-12-02 11:54:24 +0100

    [UPDATE] Acolyte update listener

--- services/sockets/socketEvent.js
+++ services/sockets/socketEvent.js
@@ -4,18 +4,12 @@ events = (socket) => {
   
     console.log({ Clientsocket: socket.id });
     socket.emit("new_user", socket.id);
-  
-    socket.on('slider', (data) => {
-      console.log(data);
-      socket.broadcast.emit('slider', data);
-    });
 
     //Update acolyte values
     socket.on('update_acolyte_values', async data => {
       try {
-        const dataObj = JSON.parse(data);
-        console.log(`EMAIL: ${dataObj.email}`)
-        const updatedUser = await userService.updateOneUser(dataObj.email, dataObj.body);
+        console.log(`EMAIL: ${data.email}`)
+        const updatedUser = await userService.updateOneUser(data.email, data.body);
         console.log(`UPDATED: ${updatedUser}`)
         socket.broadcast.emit('update_acolyte_values', updatedUser);
       } catch (error) {
@@ -23,26 +17,6 @@ events = (socket) => {
         socket.emit('update_acolyte_valuesError', error);
       }
     });
-
-    // TEST BROADCAST
-    socket.on('test_broadcast', async (data) => {
-      try {
-        socket.broadcast.emit('test_broadcast', data);
-      } catch (error) {
-        console.log(error);
-        socket.emit('test_broadcastError', error);
-      }
-    });
-  
-    //Enter Crypt
-    socket.on('crypt-enter', data => {
-      try {
-        const response = //await al service
-        socket.emit('crypt-enter', acolit)
-      } catch (error) {
-        
-      }
-    })
   
     socket.on('disconnect', () => {
       console.log('Client disconnected: ', socket.id);      
