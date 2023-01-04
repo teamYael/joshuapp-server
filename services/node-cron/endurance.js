3d613ad7e9eb477249797c142ddb7afc15b9c4fb
Josu <josu.echeverria@ikasle.aeg.eus>
2023-01-04 12:39:14 +0100

    [FIX] Launch node-cron twice

--- services/node-cron/endurance.js
+++ services/node-cron/endurance.js
@@ -1,23 +1,24 @@
 const userService = require("../../src/services/userService");
-const cron = require('node-cron');
-const server = require('../../src/index');
+const cron = require("node-cron");
+const server = require("../../src/index");
 const io = server.socketIO;
 
 const enduranceNodeCron = async () => {
   // Node-cron to update endurance and concentration
-    cron.schedule("0 0 */1 * * *", async () => {
-        try {
-          const updatedAcolytes = await userService.updateAcolytesEnduranceAndConcentration();
-          const acolytesStateUpdated = await userService.updateAcolytesState();
-          const acolytes = await userService.getAcolitsUsers();
-          console.log(acolytes)
-          io.emit('node_cron', acolytes);
-        } catch (error) {
-          console.log(error);
-        }
-    });
-}
+  cron.schedule("0 0 */1 * * *", async () => {
+    try {
+      const updatedAcolytes =
+        await userService.updateAcolytesEnduranceAndConcentration();
+      const acolytesStateUpdated = await userService.updateAcolytesState();
+      const acolytes = await userService.getAcolitsUsers();
+      console.log(acolytes);
+      io.emit("node_cron", acolytes);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
 
 module.exports = {
-    enduranceNodeCron
-}
+  enduranceNodeCron,
+};
--- src/index.js
+++ src/index.js
@@ -1,60 +1,60 @@
-require('dotenv').config();
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const mongodbRoute = `mongodb+srv://${process.env.DBUSERNAME}:${process.env.PASSWORD}@e4p1.gye2wcf.mongodb.net/JoshuApp`;
 //Josu `mongodb+srv://${process.env.DBUSERNAME}:${process.env.PASSWORD}@e4p1.gye2wcf.mongodb.net/JoshuApp'
 //Unai: 'mongodb+srv://${process.env.DBUSERNAME}:${process.env.PASSWORD}@cluster0.j8erkam.mongodb.net/JoshuApp'
 //Yael: `mongodb+srv://${process.env.DBUSERNAME}:${process.env.PASSWORD}@e4p1.zrnbvtk.mongodb.net/JoshuApp`
 //Oier: `mongodb+srv://${process.env.PASSWORD}:${process.env.PASSWORD}@e4p1.bntdsno.mongodb.net/JoshuApp`
 
-const userRouter = require('./routes/userRoutes');
-const dollRouter = require('./routes/dollRoutes');
+const userRouter = require("./routes/userRoutes");
+const dollRouter = require("./routes/dollRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = socketIO(server, {
   pingTimeout: 30000,
-  cors : {
-      origin: '*',
-  }
+  cors: {
+    origin: "*",
+  },
 });
 exports.socketIO = io;
 
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
-})
+});
 app.use("/api", userRouter);
 app.use("/api", dollRouter);
 
 async function start() {
   try {
     await mongoose.connect(mongodbRoute);
-    app.get('/', (req, res) => {
-      res.send('Welcome to Absentia Staging Socket Service from CI')
-    })
+    app.get("/", (req, res) => {
+      res.send("Welcome to Absentia Staging Socket Service from CI");
+    });
     server.listen(PORT, () => {
       console.log(`API is listening on port ${PORT}`);
     });
     console.log(`Conexión con Mongo correcta.`);
   } catch (error) {
-    console.log(process.env)
+    console.log(process.env);
     console.log(`Error al conectar a la base de datos: ${error.message}`);
   }
 }
 
 start();
 
-require('../services/sockets/socketMain');
-const { enduranceNodeCron } = require('../services/node-cron/endurance');
+require("../services/sockets/socketMain");
+const { enduranceNodeCron } = require("../services/node-cron/endurance");
 
 // Function to initialize node-cron
 enduranceNodeCron();
