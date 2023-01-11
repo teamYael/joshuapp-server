c918bb6345e783655347d94dc0e6f7b6865f7fa7
UNAI ALFARO GARCIA <unai.alfaro@ikasle.aeg.eus>
2023-01-11 21:40:40 +0100

    [UPDATE] updatePoison change with return

--- src/database/User.js
+++ src/database/User.js
@@ -184,12 +184,11 @@ const getConnectedJoshuaUsersIdSocket = async () => {
 
 const updateToPoison = async () => {
   try {
-    const updatedAcolytes = await User.updateMany(
-      { genre: { $eq: "male" } },
+     const updatedAcolytes = await User.updateMany(
+      { genre: { $eq: "male" }, joshua:{ $eq: false } },
       { $set: { isPoison: true} },
       { new: true }
     );
-    return updatedAcolytes;
   } catch (error) {
     throw error;
   }
