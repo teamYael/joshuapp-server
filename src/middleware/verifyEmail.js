aa50317a771061f3a335663fbb7b5bc9fb1ccbf3
Josu <josu.echeverria@ikasle.aeg.eus>
2023-01-24 20:28:35 +0100

    [UPDATE] Token and email middlewares

--- src/middleware/verifyEmail.js
+++ src/middleware/verifyEmail.js
@@ -1,15 +1,20 @@
-const verifyEmail = async (email) => {
+const verifyEmail = async (req, res, next) => {
+  const email = req.body.claims.email;
   const aegStudentEmail = "@ikasle.aeg.eus";
   const aegTeacherEmail = "@aeg.eus";
 
   if (email === process.env.ROL_JOSHUA_GROUP) {
-    return true;
+    return next();
   }
 
   if (email.includes(aegStudentEmail) || email.includes(aegTeacherEmail)) {
     console.log(`${email} is a valid email`);
-    return true;
+    return next();
   }
-  return false;
+  return res.status(401).send({
+    status: "FAILED",
+    message: "invalid email",
+  });
 };
+
 exports.verifyEmail = verifyEmail;
