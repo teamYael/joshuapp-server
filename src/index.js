133bac694589c3e078b0301fd9ef8238ddf85421
Yael Martinez <yael.martinez@ikasle.aeg.eus>
2023-01-25 07:55:39 +0100

    fly.toml change

--- src/index.js
+++ src/index.js
@@ -57,6 +57,7 @@ start();
 require("../services/sockets/socketMain");
 
 // const { enduranceNodeCron } = require("../services/node-cron/endurance");
-
 // // Function to initialize node-cron
 // enduranceNodeCron();
+
+
