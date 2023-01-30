d3d809a25f8b78709a4a5e838b5cef753de07f48
UNAI ALFARO GARCIA <unai.alfaro@ikasle.aeg.eus>
2023-01-30 13:37:31 +0100

    Fix conflicts

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { verifyToken } = require('../middleware/verifyToken');
-const { verifyEmail } = require('../middleware/verifyEmail');
-const { authenticateToken } = require('../middleware/verifyJWT')
+const { verifyToken } = require("../middleware/verifyToken");
+const { verifyEmail } = require("../middleware/verifyEmail");
+const { authenticateToken } = require("../middleware/verifyJWT");
 
 const userController = require("../controllers/userController");
 
-router.get("/data/:userEmail", authenticateToken, userController.getInitialData);
+router.get(
+  "/data/:userEmail",
+  authenticateToken,
+  userController.getInitialData
+);
 router.get("/acolits", userController.getAcolitsUsers);
 router.post("/token", verifyToken, verifyEmail, userController.loginUser);
+router.patch("/user/:userEmail", userController.updateUserTokens);
 router.patch("/users/:userEmail", userController.updateOneUser);
 router.patch("/acolitoncript/:userEmail", userController.updateOnCrypt);
 
