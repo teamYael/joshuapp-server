c1ba132c0b5b064301aea5b38e27090b21fd209b
UnaiAlfaroAEG <unai.alfaro@ikasle.aeg.eus>
2023-01-31 10:16:05 +0100

    [UPDATE] middleware if change

--- services/sockets/socketMain.js
+++ services/sockets/socketMain.js
@@ -6,8 +6,8 @@ const socketEvents = require('./socketEvent').socketEvents;
 
 io.use(function (socket, next) {
     if(socket.handshake.query && socket.handshake.query.token) {
-        console.log(`Istvan: ${socket.handshake.itsvan}`)
-        if(socket.handshake.itsvan===true){
+        console.log(`Istvan: ${socket.handshake.itsvan.isItsvan}`)
+        if(socket.handshake.itsvan.isItsvan === true){
             jwt.verify(socket.handshake.query.token, process.env.ACCESS_TOKEN_SECRET_NOT_EXPIRED, (error, decoded) => {
                 if (error) {
                     return next(new Error("Authentication error"));
