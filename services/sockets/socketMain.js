df0bfb3d21406ead8067253b20b332530f636665
UnaiAlfaroAEG <unai.alfaro@ikasle.aeg.eus>
2023-01-31 10:40:56 +0100

    conosle.log(+1)

--- services/sockets/socketMain.js
+++ services/sockets/socketMain.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const socketEvents = require('./socketEvent').socketEvents;
 
 io.use(function (socket, next) {
+    console.log(`Istvan fuera: ${socket.handshake.itsvan.isItsvan}`)
     if(socket.handshake.query && socket.handshake.query.token) {
         console.log(`Istvan dentro: ${socket.handshake.itsvan.isItsvan}`)
         if(socket.handshake.itsvan.isItsvan === true){
