8431208d39e3951de7cf789e676bf651622f604d
UnaiAlfaroAEG <unai.alfaro@ikasle.aeg.eus>
2023-01-31 10:45:05 +0100

    [UPDATE] console log +2

--- services/sockets/socketMain.js
+++ services/sockets/socketMain.js
@@ -5,9 +5,9 @@ const jwt = require('jsonwebtoken');
 const socketEvents = require('./socketEvent').socketEvents;
 
 io.use(function (socket, next) {
-    console.log(`Istvan fuera: ${socket.handshake.itsvan.isItsvan}`)
+    console.log(`Istvan fuera: ${socket.handshake.itsvan}`)
     if(socket.handshake.query && socket.handshake.query.token) {
-        console.log(`Istvan dentro: ${socket.handshake.itsvan.isItsvan}`)
+        console.log(`Istvan dentro: ${socket.handshake.itsvan}`)
         if(socket.handshake.itsvan.isItsvan === true){
             jwt.verify(socket.handshake.query.token, process.env.ACCESS_TOKEN_SECRET_NOT_EXPIRED, (error, decoded) => {
                 if (error) {
