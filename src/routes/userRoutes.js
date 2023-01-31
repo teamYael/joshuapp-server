bdf19bf0490d062697a6b6606675b65706b78660
OIER ANDUEZA ARTOLA <oier.andueza@ikasle.aeg.eus>
2023-01-31 08:49:31 +0100

    [UPDATE] added to a route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { verifyToken } = require("../middleware/verifyToken");
 const { verifyEmail } = require("../middleware/verifyEmail");
 const { authenticateToken } = require("../middleware/verifyJWT");
+const { authenticateRefreshToken } = require("../middleware/verifyRefreshJWT")
 
 const userController = require("../controllers/userController");
 
@@ -13,7 +14,7 @@ router.get(
 );
 router.get("/acolits", userController.getAcolitsUsers);
 router.post("/token", verifyToken, verifyEmail, userController.loginUser);
-router.patch("/user/:userEmail", userController.updateUserTokens);
+router.patch("/user/:userEmail", authenticateRefreshToken, userController.updateUserTokens);
 router.patch("/users/:userEmail", userController.updateOneUser);
 router.patch("/acolitoncript/:userEmail", userController.updateOnCrypt);
 
